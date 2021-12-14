cd79fefe5180c979891e5c3fe07d226d72079c90
Andrei Kislitsyn <kisandy@yandex.ru>
2021-12-14 13:45:19 +0300

    fix layers

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.andreikingsley"
-version = "0.1.2-dev-1.1"
+version = "0.1.2-dev-1.2"
 
 repositories {
     mavenCentral()
@@ -28,7 +28,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.andreikingsley"
             artifactId = "ggdsl"
-            version = "0.1.2-dev-1.1"
+            version = "0.1.2-dev-1.2"
 
             from(components["java"])
         }
--- src/main/kotlin/com/andreikingsley/ggdsl/dsl/layers.kt
+++ src/main/kotlin/com/andreikingsley/ggdsl/dsl/layers.kt
@@ -2,16 +2,16 @@ package com.andreikingsley.ggdsl.dsl
 
 import com.andreikingsley.ggdsl.ir.Geom
 
-fun PlotContext.points(block: LayerContext.() -> Unit) {
+fun PlotContext.points(block: PointsContext.() -> Unit) {
     // TODO dataset
     layers.add(PointsContext().apply { copyFrom(this@points) }.apply(block).toLayer(Geom.POINT))
 }
 
-fun PlotContext.bars(block: LayerContext.() -> Unit) {
+fun PlotContext.bars(block: BarsContext.() -> Unit) {
     // TODO dataset
     layers.add(BarsContext().apply { copyFrom(this@bars) }.apply(block).toLayer(Geom.BAR))
 }
 fun PlotContext.line(block: LayerContext.() -> Unit) {
     // TODO dataset
-    layers.add(LineContext().apply { copyFrom(this@line) }.apply(block).toLayer(Geom.LINE))
+    layers.add(LayerContext().apply { copyFrom(this@line) }.apply(block).toLayer(Geom.LINE))
 }
