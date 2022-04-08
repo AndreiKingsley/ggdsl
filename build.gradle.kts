410a143503bc9af7633dedfbd8b207114f5151eb
Andrei <kisandy@yandex.ru>
2022-04-09 01:14:34 +0400

    debug

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.andreikingsley"
-val ggDSLVersion = "0.3.1-9"
+val ggDSLVersion = "0.3.1-9-debug"
 version = ggDSLVersion
 
 repositories {
--- src/main/kotlin/com/andreikingsley/ggdsl/dsl/contexts.kt
+++ src/main/kotlin/com/andreikingsley/ggdsl/dsl/contexts.kt
@@ -46,6 +46,7 @@ inline infix fun<DomainType : Any, RangeType: Any> NonPositionalMapping<DomainTy
 ){
     scale = NonPositionalScaleContext<DomainType, RangeType>(domainType, rangeType).apply(block).let {
         it.scale.apply {
+            println((it as CategoricalNonPositionalScale<*, *>).values)
             this.legend = it.legend
         }
     }
