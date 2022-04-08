d33365cca3b275fb27b1b866bdfc30d27e9f86e1
Andrei <kisandy@yandex.ru>
2022-04-08 23:16:46 +0400

    add scalable mapping interface

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.andreikingsley"
-val ggDSLVersion = "0.3.1-2"
+val ggDSLVersion = "0.3.1-3"
 version = ggDSLVersion
 
 repositories {
--- src/main/kotlin/com/andreikingsley/ggdsl/ir/bindings/mapping.kt
+++ src/main/kotlin/com/andreikingsley/ggdsl/ir/bindings/mapping.kt
@@ -7,15 +7,18 @@ import com.andreikingsley.ggdsl.ir.scale.PositionalScale
 import com.andreikingsley.ggdsl.ir.scale.Scale
 import kotlin.reflect.KType
 
-sealed interface Mapping
+sealed interface Mapping<DomainType> {
+    val aes: Aes
+    val source: DataSource<DomainType>
+}
 
 sealed interface ScalableMapping {
     val scale: Scale
 }
 
-sealed interface PositionalMapping<DomainType>: Mapping {
-    val aes: PositionalAes
-    val source: DataSource<DomainType>
+sealed interface PositionalMapping<DomainType>: Mapping<DomainType> {
+    override val aes: PositionalAes
+    override val source: DataSource<DomainType>
     val domainType: KType
 }
 
@@ -34,12 +37,12 @@ data class ScalablePositionalMapping<DomainType : Any>(
 ): PositionalMapping<DomainType>, ScalableMapping
 
 data class NonPositionalMapping<DomainType : Any, RangeType : Any>(
-    val aes: NonPositionalAes<RangeType>,
-    val source: DataSource<DomainType>,
+    override val aes: NonPositionalAes<RangeType>,
+    override val source: DataSource<DomainType>,
     val domainType: KType,
     val rangeType: KType,
 
     override var scale: NonPositionalScale<DomainType, RangeType>
-): Mapping, ScalableMapping
+): Mapping<DomainType>, ScalableMapping
 
 // todo nonscalable???s
